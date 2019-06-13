c2c0fcd29e513f0392e80471a3be736386ff7584
jinhegithub <smisso71479@outlook.com>
2019-06-13 11:06:37 +0800

    eslint prettier setting

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,9 @@
 module.exports = {
   env: {
     browser: true,
+    serviceworker: true,
     commonjs: true,
     es6: true,
-    mocha: true,
-    node: false
   },
   parserOptions: {
     ecmaFeatures: {
@@ -12,7 +11,6 @@ module.exports = {
     },
     sourceType: "module"
   },
-  plugins: ["react"],
   rules: {
     "no-const-assign": "warn",
     "no-this-before-super": "warn",
@@ -20,7 +18,30 @@ module.exports = {
     "no-unreachable": "warn",
     "no-unused-vars": "warn",
     "constructor-super": "warn",
-    "valid-typeof": "warn"
+    "valid-typeof": "warn",
+    "react/prefer-stateless-function": 0,
+    "react/jsx-filename-extension": 0,
+    "react/jsx-one-expression-per-line": 0,
+    "no-use-before-define": 0,
+    "no-param-reassign": 0,
+    "react/prop-types": 0
   },
-  extends: ["eslint:recommended", "plugin:react/recommended"]
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "react-app",
+    "airbnb",
+    "standard",
+    "prettier",
+    "prettier/flowtype",
+    "prettier/react",
+    "prettier/standard"
+  ],
+  plugins: [
+    "react",
+    "flowtype",
+    "prettier",
+    "standard"
+  ]
 };
--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import React from "react";
+import React, { Component } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World!!!</h1>
-    </div>
-  );
+export default class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1> Hello world 1 </h1>{" "}
+      </div>
+    );
+  }
 }
-
-export default App;
